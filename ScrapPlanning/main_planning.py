ad4128ff0f023eae59ae2bb977efeef1399d0071
AbdallahSlimane <slimane.abdallah75@gmail.com>
2023-06-24 20:14:21 +0200

    feature/abdallah: scraping completed (I hope)

--- ScrapPlanning/main_planning.py
+++ ScrapPlanning/main_planning.py
@@ -6,7 +6,11 @@ from myges_scrap_planning import MyGesScrapPlanning
 def main():
     scraper = MyGesScrapPlanning("aslimane1", "Hajar77176")
     scraper.login()
+    scraper.navigate_to_planning()
     scraper.scrape_planning()
+    scraper.next_week()
+    scraper.scrape_planning()
+    scraper.close()
 
 
 if __name__ == "__main__":
--- ScrapPlanning/myges_scrap_planning.py
+++ ScrapPlanning/myges_scrap_planning.py
@@ -1,11 +1,11 @@
 import json
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 
 class MyGesScrapPlanning:
@@ -24,9 +24,29 @@ class MyGesScrapPlanning:
         password_box.send_keys(Keys.RETURN)
         time.sleep(2)
 
-    def scrape_planning(self):
+    def navigate_to_planning(self):
         self.driver.get("https://myges.fr/student/planning-calendar")
+        time.sleep(2)
 
+    def scrape_event_details(self):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fc-event-inner")))
+            event_elements = self.driver.find_elements(By.CSS_SELECTOR, ".fc-event-inner")
+        except TimeoutException:
+            print("No events found after waiting for 10 seconds.")
+            return []
+
+        events = []
+        for event in event_elements:
+            hour = event.find_element(By.CSS_SELECTOR, ".fc-event-time").text.replace('\n', ' ')
+            title = event.find_element(By.CSS_SELECTOR, ".fc-event-title").text.replace('\n', ' ')
+            events.append({
+                "Heures": hour,
+                "Cours": title
+            })
+        return events
+
+    def scrape_planning(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'calendar:currentWeek')))
         current_week_element = self.driver.find_element(By.ID, 'calendar:currentWeek')
         current_week_text = current_week_element.text.replace('\n', ' ')
@@ -39,18 +59,13 @@ class MyGesScrapPlanning:
         header_cells = header_row.find_elements(By.TAG_NAME, "th")
         header_text = [cell.text for cell in header_cells]
 
-        body_rows = self.driver.find_elements(By.CSS_SELECTOR, "table.fc-agenda-slots tbody tr")
-        body_text = []
-        for row in body_rows:
-            time_cell = row.find_element(By.TAG_NAME, "th")
-            time_text = time_cell.text
-            body_text.append(time_text)
+        events = self.scrape_event_details()
 
         data = {
             "Semaine Courante": current_week_text,
             "Dernière mise à jour": last_update_text,
             "Jour": header_text,
-            "Heure": body_text,
+            "Evenements": events,
         }
 
         print(data)
@@ -62,7 +77,10 @@ class MyGesScrapPlanning:
         print("Data saved to planning.json")
         time.sleep(5)
 
+    def next_week(self):
+        next_week_button = self.driver.find_element(By.ID, "calendar:nextMonth")
+        next_week_button.click()
+        time.sleep(5)
+
     def close(self):
         self.driver.quit()
-
-
